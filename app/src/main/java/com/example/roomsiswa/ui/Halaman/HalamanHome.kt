45b45c2edb67d4fac67dc59567d514fe45bb760c
mhmdfakhrim <muhfakhri4@gmail.com>
2023-12-04 00:24:49 +0700

    Activity

--- app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanHome.kt
@@ -1,5 +1,6 @@
 package com.example.roomsiswa.ui.Halaman
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -11,6 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
@@ -57,4 +59,13 @@ fun HomeScreen(
                 }
             },
     ){
+        innerPadding ->
+        val uiStateSiswa by viewModel.homeUiState.collectAsState()
+        BodyHome(
+            itemSiswa = uiStateSiswa.listSiswa,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
     }
+}
