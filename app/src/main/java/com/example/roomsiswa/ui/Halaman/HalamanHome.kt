617e7adb68fe4d58015f71beff97d897f3400c9c
mhmdfakhrim <muhfakhri4@gmail.com>
2023-12-04 00:34:30 +0700

    Activity

--- app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanHome.kt
@@ -3,6 +3,8 @@ package com.example.roomsiswa.ui.Halaman
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -92,6 +94,23 @@ fun BodyHome(
             ListSiswa(
                 itemSiswa = itemSiswa,
                 modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
+
+@Composable
+fun ListSiswa(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier=Modifier
+){
+    LazyColumn(modifier = Modifier){
+        items(items = itemSiswa, key = {it.id}){
+            person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
             )
         }
     }
