5c1e12510bbee30c6908bb9451cc0efd5b96d8c3
mhmdfakhrim <muhfakhri4@gmail.com>
2023-12-03 22:46:55 +0700

    Activity

---
+++ app/src/main/java/com/example/roomsiswa/ui/Halaman/HalamanEntry.kt
@@ -0,0 +1,24 @@
+package com.example.roomsiswa.ui.Halaman
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomsiswa.model.EntryViewModel
+import com.example.roomsiswa.model.PenyediaViewModel
+import com.example.roomsiswa.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
